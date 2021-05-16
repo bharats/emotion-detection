89302df597d0b498fa7fe7acab3ddcbd2d7f2a10
LKWaters <48098727+LKWaters@users.noreply.github.com>
2021-05-16 17:37:25 -0400

    Added Decomposition and Classification Options

--- src/RealTimeEmotion.py
+++ src/RealTimeEmotion.py
@@ -1,58 +1,131 @@
 import cv2
-from PIL import Image
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.decomposition import PCA
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import confusion_matrix, classification_report
+from tkinter import *
+from tkinter.ttk import *
 from PIL import Image
-
-total = pd.read_csv('icml_face_data.csv')
-total = total.drop(columns = [' Usage'])
-
-total_y = total['emotion']
-total_x = total[' pixels'].str.split(' ',expand=True).astype(dtype = 'uint8')
-
-x_train, x_test, y_train, y_test  = train_test_split(total_x, total_y, test_size=0.25, random_state=42)
+import pickle
 
 
+decomposition = " "
+classifier = " "
+
+#---------------------------------------------------------------------------
+
+options1 = [
+    "Decomposition",
+    "PCA",
+    "NMF",
+    "Kernel PCA"
+]
+options2 = [
+    "Classifier",
+    "Logistic Regression",
+    "Linear SVC",
+    "Multilater Perceptron",
+    "Random Forrest",
+    "K-Nearest Neighbor"
+]
+
+def choose():
+    global decomposition
+    global classifier
+    decomposition = clicked1.get()
+    classifier = clicked2.get()
+    root.destroy()
+
+root = Tk()
+root.geometry("300x100")
+
+clicked1 = StringVar(root)
+clicked1.set("Decomposition")
+
+clicked2 = StringVar(root)
+clicked2.set("Classifier")
+
+drop1 = OptionMenu(root, clicked1, *options1)
+drop1.pack()
+
+drop2 = OptionMenu(root, clicked2, *options2)
+drop2.pack()
+
+button = Button(root, text="Start", command = choose).pack()
+
+root.mainloop()
 
 
+#----------------------------------------------------------------------------------
 
+x_train_file = open('../data/processed/data_models/x_train.pkl', 'rb')
+x_train = pickle.load(x_train_file)
 
-
+x_sc_train = open('../data/processed/data_models/x_train_sc.pkl', 'rb')
+x_train_sc =pickle.load(x_sc_train)
 
 sc = MinMaxScaler()
-x_train_sc = sc.fit_transform(x_train)
 save_sc = sc.fit(x_train)
 
-pca = PCA(n_components=255).fit(x_train_sc)
-pca_save = PCA(n_components=255)
-pca_s = pca_save.fit(x_train_sc)
+
+if(decomposition =="PCA"):
+    # pca
+    text = 'pca'
+    pca_file = open('../data/processed/data_models/x_train_pca.pkl', 'rb')
+    x_train_m = pickle.load(pca_file)
+
+    dec_file = open('../data/processed/data_models/pca.pkl', 'rb')
+    dec = pickle.load(dec_file)
+
+elif(decomposition == "NMF"):
+    # nmf
+    text = 'nmf'
+    nmf_file = open('../data/processed/data_models/x_train_nmf.pkl', 'rb')
+    x_train_m = pickle.load(nmf_file)
+
+    dec_file = open('../data/processed/data_models/nmf.pkl', 'rb')
+    dec = pickle.load(dec_file)
+
+else:
+    # kernel pca
+    text = 'kpca'
+    kpca_file = open('../data/processed/data_models/x_train_kpca.pkl', 'rb')
+    x_train_m = pickle.load(kpca_file)
+
+    dec_file = open('../data/processed/data_models/kpca.pkl', 'rb')
+    dec = pickle.load(dec_file)
 
 
-x_train_pca = pca.transform(x_train_sc)
-
-lrc = LogisticRegression(C=0.01, penalty='l2', solver='saga', tol=0.0001, max_iter=800)
-
-pipe = Pipeline([('pca', pca), ('logistic', lrc)])
-pipe.fit(x_train_pca, y_train)
 
 
+if(classifier == "Logistic Regression"):
+    # logistic regression
+    dec_class_file = open('../data/processed/data_models/lrc_'+text+'.pkl', 'rb')
+    classif =pickle.load(dec_class_file)
+
+elif(classifier == "Linear SVC"):
+    # support vector machine classifier
+    dec_class_file = open('../data/processed/data_models/scv_' + text + '.pkl', 'rb')
+    classif = pickle.load(dec_class_file)
+
+elif (classifier == "Multilater Perceptron"):
+    # multilater perceptron
+    dec_class_file = open('../data/processed/data_models/mlp_' + text + '.pkl', 'rb')
+    classif = pickle.load(dec_class_file)
+
+elif (classifier == "Random Forrest"):
+    # random forrest
+    dec_class_file = open('../data/processed/data_models/rfc_' + text + '.pkl', 'rb')
+    classif = pickle.load(dec_class_file)
+
+else:
+    # k-nearest neighbor
+    dec_class_file = open('../data/processed/data_models/knc_' + text + '.pkl', 'rb')
+    classif = pickle.load(dec_class_file)
 
 
-
-
-
-
-
-#--------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
 
 cap = cv2.VideoCapture(1) #default is normally 0 but my webcam is 1
-cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cascade = cv2.CascadeClassifier('../data/raw/haarcascade_frontalface_default.xml')
 
 while True:
     ret, frame = cap.read()
@@ -63,16 +136,15 @@ while True:
         (x,y,w,h) = detections[0]
         frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-        face = Image.fromarray(gray[y-10:y+h+10,x-10:x+w+10])
+        face = Image.fromarray(gray[y:y+h,x:x+w])
         face = face.resize((48, 48), Image.ANTIALIAS)
         face = np.resize(np.array(face), (1,2304))
 
         face_sc = save_sc.transform(face)
-        face_pca = pca_s.transform(face_sc)
-        pred = pipe.predict(face_pca)
+        face_dec = dec.transform(face_sc)
+        pred = classif.predict(face_dec)
 
         emotion = {0:"Angry",1:"Disgust",2:"Fear",3:"Happy",4:"Sad",5:"Suprised", 6:"Neutral"}
-
 
         cv2.putText(frame,str(emotion.get(pred[0])),(x, y-10),cv2.FONT_HERSHEY_SIMPLEX, 1,(255, 0, 0),2,cv2.LINE_4)
 
